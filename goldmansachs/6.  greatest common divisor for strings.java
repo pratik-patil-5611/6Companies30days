fc67d465a75c36d10ad2f597a7e9358f83586801
Pratik Kumar Patil <112pratik@gmail.com>
2022-01-03 21:25:52 +0530

    Create 6.  greatest common divisor for strings.java

---
+++ goldmansachs/6.  greatest common divisor for strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1))
+            return "";
+        else if(str1.equals(str2))
+            return str1;
+        else if(str1.length()>str2.length())
+            return gcdOfStrings(str1.substring(str2.length()),str2);
+        else
+            return gcdOfStrings(str2.substring(str1.length()),str1);
+    }
+}
